ecc709f79e620102e018df065291c2977f02f120
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-26 21:42:54 -0500

    Job 5

---
+++ src/main/kotlin/ElementJobMultipleChildren2.kt
@@ -0,0 +1,89 @@
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// Cancel nieto
+fun main() {
+    log("Start")
+
+    runBlocking {
+        var childJob1: Job? = null
+        var childJob2: Job? = null
+        var childChildJob: Job? = null
+        val job = launch {
+
+            childJob1 = launch {
+
+                childChildJob = launch {
+                    repeat(5) {
+                        log("Child1-Child rep #$it : I'm active")
+                        delay(1000)
+                    }
+                    log("Child1-Child : I'm finishing.")
+                }
+
+                repeat(5) {
+                    log("Child 1 rep #$it : I'm active")
+                    delay(1000)
+                }
+                log("Child 1 : I'm finishing.")
+            }
+
+            childJob2 = launch {
+                repeat(5) {
+                    log("Child 2 rep #$it : I'm active")
+                    delay(1000)
+                }
+                log("Child 2 : I'm finishing.")
+            }
+
+            log("Launch : I'm finishing.")
+        }
+
+        delay(1500)
+        while (job.isActive) {
+            log("RunBlocking : Job is active")
+            delay(1000)
+
+            if (childChildJob?.isActive == true) {
+                log("RunBlocking : Cancelling Child-Child Job")
+                childChildJob?.cancel()
+            }
+        }
+
+        if (childJob1?.isCancelled == true) {
+            log("RunBlocking : Child Job 1 is cancelled")
+        }
+
+        if (childJob1?.isCompleted == true) {
+            log("RunBlocking : Child Job 1 is completed")
+        }
+
+        if (childChildJob?.isCancelled == true) {
+            log("RunBlocking : Child-Child Job is cancelled")
+        }
+
+        if (childChildJob?.isCompleted == true) {
+            log("RunBlocking : Child-Child Job is completed")
+        }
+
+        if (childJob2?.isCancelled == true) {
+            log("RunBlocking : Child Job 2 is cancelled")
+        }
+
+        if (childJob2?.isCompleted == true) {
+            log("RunBlocking : Child Job 2 is completed")
+        }
+
+        if (job.isCancelled) {
+            log("RunBlocking : Job is cancelled")
+        }
+
+        if (job.isCompleted) {
+            log("RunBlocking : Job is completed")
+        }
+    }
+
+    log("End")
+}
