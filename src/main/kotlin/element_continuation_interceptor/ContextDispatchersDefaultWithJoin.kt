575a2742ba46a22658cee56b9740f98b500a7bb0
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-27 06:39:01 -0500

    Context Dispatchers

---
+++ src/main/kotlin/element_continuation_interceptor/ContextDispatchersDefaultWithJoin.kt
@@ -0,0 +1,31 @@
+package element_continuation_interceptor
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import log
+import kotlin.coroutines.ContinuationInterceptor
+
+fun main() {
+    log("Start")
+
+    runBlocking {
+
+        launch (Dispatchers.Default) {
+
+            log("Parent-Before | ContinuationInterceptor: ${this.coroutineContext[ContinuationInterceptor]}")
+
+            val job = launch {
+                log("Child | ContinuationInterceptor: ${this.coroutineContext[ContinuationInterceptor]}")
+            }
+
+            job.join()
+
+            log("Parent-After | ContinuationInterceptor: ${this.coroutineContext[ContinuationInterceptor]}")
+
+        }
+
+    }
+
+    log("End")
+}
