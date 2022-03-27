db4d39714ea665f4190debbc4517aa0c98af81f2
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2022-03-26 21:39:07 -0500

    Create ElementDeferredJoinCancel2.kt

---
+++ src/main/kotlin/ElementDeferredJoinCancel2.kt
@@ -0,0 +1,45 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    log("Start")
+
+    runBlocking {
+        val result = async {
+            log("Async : I'm starting.")
+            val a = (1..50).random()
+            log("Async : A = $a")
+            delay(1000)
+            val b = (1..50).random()
+            log("Async : B = $b")
+            delay(1000)
+            log("Async : I'm finishing.")
+            a + b
+        }
+
+        launch {
+            delay(1500)
+            log("Before cancelling Job")
+            result.cancel()
+        }
+
+        try {
+            log("RunBlocking : Before calling .await()")
+            log("RunBlocking : Result is ${result.await()}")
+        } catch (e: Exception) {
+            log("RunBlocking : Oops! There was a problem: ${e.message}")
+        }
+
+        if (result.isCancelled) {
+            log("RunBlocking : Job is cancelled")
+        }
+
+        if (result.isCompleted) {
+            log("RunBlocking : Job is completed")
+        }
+    }
+
+    log("End")
+}
